aab0f00fe08bd15ac5f72212603b4ef883d844a6
jondpugh <60925021+jondpugh@users.noreply.github.com>
2022-09-19 17:49:11 +0100

    Update run.sh
    This works, Removing Debug

--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-echo "TEST"
-
 chmod +x carbon/tools/NStrip
 
-echo "TEST PERM CHANGED"
-
 ./carbon/tools/NStrip -p -cg --keep-resources -n --unity-non-serialized ./RustDedicated_Data/Managed/Assembly-CSharp.dll ./RustDedicated_Data/Managed/Assembly-CSharp.dll
-
-echo "TEST PATCHING DONE"
 
 ./RustDedicated -batchmode +server.port $1 +server.identity "rust" +rcon.port $2 +rcon.web true +server.hostname "${3}" +server.level "${4}" +server.description "${5}" +server.url ${6} +server.headerimage ${7} +server.logoimage $8 +server.maxplayers $9 +rcon.password ${10} +server.saveinterval ${11} +app.port ${12} $( [ -z ${16} ] && printf %s "+server.worldsize \"${13}\" +server.seed \"${14}\"" || printf %s "+server.levelurl " ) ${15}
