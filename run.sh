152a26805a973b7968b82a3b225b48b0cf776815
Jon Pugh <60925021+jondpugh@users.noreply.github.com>
2022-11-26 15:36:44 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-set -x
-trap read debug
-echo "______________Stage 1______________"
 ./carbon/tools/environment.sh
-
-echo "______________Stage 2______________"
 source carbon/tools/environment.sh
-
-echo "______________Stage 3______________"
-./RustDedicated -batchmode +server.port $1 +server.identity "rust" +rcon.port $2 +rcon.web true +server.hostname "${3}" +server.level "${4}" +server.description "${5}" +server.url ${6} +server.headerimage ${7} +server.logoimage $8 +server.maxplayers $9 +rcon.password ${10} +server.saveinterval ${11} +app.port ${12} $( [ -z ${16} ] && printf %s "+server.worldsize \"${13}\" +server.seed \"${14}\"" || printf %s "+server.levelurl " ) ${15}
+# ./RustDedicated -batchmode +server.port $1 +server.identity "rust" +rcon.port $2 +rcon.web true +server.hostname "${3}" +server.level "${4}" +server.description "${5}" +server.url ${6} +server.headerimage ${7} +server.logoimage $8 +server.maxplayers $9 +rcon.password ${10} +server.saveinterval ${11} +app.port ${12} $( [ -z ${16} ] && printf %s "+server.worldsize \"${13}\" +server.seed \"${14}\"" || printf %s "+server.levelurl " ) ${15}
