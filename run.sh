c44d97fc29b56cf7a04e13c26a14ff9b5ec35ee2
Jon Pugh <60925021+jondpugh@users.noreply.github.com>
2022-12-07 22:50:38 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 source carbon/tools/environment.sh
 
-print ${15}
+echo ${15}
 
-print ${15}
+echo ${15}
 
-print ${15}
+echo ${15}
 
 
 ./RustDedicated -batchmode +server.port $1 +server.identity "rust" +rcon.port $2 +rcon.web true +server.hostname "${3}" +server.level "${4}" +server.description "${5}" +server.url ${6} +server.headerimage ${7} +server.logoimage $8 +server.maxplayers $9 +rcon.password ${10} +server.saveinterval ${11} +app.port ${12} $( [ -z ${16} ] && printf %s "+server.worldsize \"${13}\" +server.seed \"${14}\"" || printf %s "+server.levelurl \"${15}\"" )
